a2b86e460f4450d52a221c41b5c5a1469440c401
RudVlad473 <andrejopa387@gmail.com>
2022-08-27 19:13:50 +0300

    Half of the content done

--- manage-landing-page/dist/js/_constants.js
+++ manage-landing-page/dist/js/_constants.js
@@ -2,9 +2,33 @@ const carousel = document.getElementById('carousel')
 const cards = document.getElementsByClassName('carousel__card')
 const cardWidth = parseInt(window.getComputedStyle(cards.item(0)).minWidth)
 const cardsCount = cards.length
+const overlay = document.getElementById('overlay')
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('to-collapse');
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('to-collapse')[0]
+toggleButton.addEventListener('click', () => {
+    
+    // if (overlay.style.display == 'none' || !overlay.style.display) {
+    //     overlay.style.display = 'block'
+    //     navbarLinks.style.paddingBlock = '3em'
+    //     navbarLinks.style.marginTop = '2em'
+    //     navbarLinks.style.marginBottom = '-4em'
+    // } else {
+    //     overlay.style = ''
+    //     navbarLinks.style = ''
+    // }
+    if (overlay.style.opacity == 0 || !overlay.style.opacity) {
+        overlay.style.opacity = '1'
+        navbarLinks.style.paddingBlock = '3em'
+        navbarLinks.style.marginTop = '2em'
+        navbarLinks.style.marginBottom = '-4em'
+    } else {
+        overlay.style = ''
+        navbarLinks.style = ''
+    }
+
+    navbarLinks.classList.toggle('active')
+})
 
 // const arrows = document.getElementsByClassName('arrow')
 // const leftArrow = arrows[0]
--- manage-landing-page/dist/js/_navbar.js
+++
@@ -1,5 +0,0 @@
-toggleButton.addEventListener('click', () => {
-    for (let i = 0; i < navbarLinks.length; i++) {
-        navbarLinks[i].classList.toggle('active');
-    }
-});
