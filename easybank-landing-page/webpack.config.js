ac2f2c4a1bdfeab67d880c9fc674985372ccdb56
RudVlad473 <andrejopa387@gmail.com>
2022-09-17 21:06:57 +0300

    Images fixed

--- easybank-landing-page/webpack.config.js
+++ easybank-landing-page/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
-        assetModuleFilename: "./images/[hash][ext][query]",
+        assetModuleFilename: "./images/[name][ext]",
     },
     module: {
         rules: [
